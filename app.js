eb13954509b7fa8db6f6ec626e4669b3fc13f5e6
Nitin Kumar <66864065+nkthehustler@users.noreply.github.com>
2022-04-08 19:07:48 -0700

    Cookie🍪 Animation
    Cookie animation popup using NodeJs as backend and GSAP Library.

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
-
-var items = ["Do Homework", "Buy groceries", "Call Sister"];
-var workItems =[];
-
+const bodyparser = require("body-parser");
 const app = express();
+
 app.use(express.static("static"));
-app.use(bodyParser.urlencoded({extended: true}))
-app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
-   var day = date.getDate();
+    res.sendFile(__dirname + "/index.html");
+});
 
-    res.render("lists", {list_title: day, newitem: items});
-})   
+app.listen(3000, function(){
+    console.log("server is listenig at port 3000");
 
-
-app.post("/", function(req,res){
-    var item = req.body.newitem;
-    items.push(item);
-    res.redirect("/")
-    
-})
-
-app.get("/work", function(req, res){
-      res.render("lists",{list_title:"Work List", newitem: workItems});
-})
-
-app.post("/work", function(req, res){
-    let item = req.body.newitem;
-    workItems.push(item);
-    res.redirect("/work");
-})
-
-
-app.listen("3000", ()=>{
-    console.log("server is listening on port 3000");
-})
+});
